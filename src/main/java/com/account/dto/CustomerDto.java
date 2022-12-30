0cfc6d784904cc2ee69e50abb542e6eb6b93818a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-30 21:50:35 +0400

    initial commit

---
+++ src/main/java/com/account/dto/CustomerDto.java
@@ -0,0 +1,17 @@
+package com.account.dto;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class CustomerDto {
+
+    private String id;
+
+    private String name;
+
+    private String surname;
+
+    private Set<CustomerAccountDto> accounts;
+}
