bc92d0615d2c95040ff5058af3b933e8466e5bd2
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-30 23:31:10 +0400

    AccountDto

--- src/main/java/com/account/dto/AccountDto.java
+++ src/main/java/com/account/dto/AccountDto.java
@@ -1,12 +1,16 @@
 package com.account.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountDto {
 
     private String id;
