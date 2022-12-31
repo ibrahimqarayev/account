939be8a3ca8331349efacc7235a08e25ed664986
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-31 20:34:53 +0400

    AccountCustomerDto.java

---
+++ src/main/java/com/account/dto/AccountCustomerDto.java
@@ -0,0 +1,20 @@
+package com.account.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountCustomerDto {
+
+    private String id;
+
+    private String name;
+
+    private String surname;
+
+
+}
