849fb8b390b5b771d4e405fad784056a9cd771bc
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-31 20:34:22 +0400

    CustomerDto

--- src/main/java/com/account/dto/CustomerDto.java
+++ src/main/java/com/account/dto/CustomerDto.java
@@ -1,10 +1,14 @@
 package com.account.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDto {
 
     private String id;
