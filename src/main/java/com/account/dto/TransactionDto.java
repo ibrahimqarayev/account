e31523d98f00f14cb6822eecf76a10dc64d93aa2
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-31 20:34:35 +0400

    TransactionDto

---
+++ src/main/java/com/account/dto/TransactionDto.java
@@ -0,0 +1,25 @@
+package com.account.dto;
+
+import com.account.constant.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDto {
+
+    private String id;
+
+    private BigDecimal amount;
+
+    private TransactionType transactionType;
+    //TransactionType transactionType = TransactionType.INITIAL;
+
+    private LocalDateTime transactionDate;
+
+}
