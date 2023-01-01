75941406c94f4c02f40813c7868cbc30d4fa1345
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-01 19:16:03 +0400

    CustomerService

---
+++ src/main/java/com/account/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.account.service;
+
+import com.account.exception.CustomerNotFoundException;
+import com.account.model.Customer;
+import com.account.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    protected Customer findById(String id) {
+        return customerRepository.findById(id).orElseThrow(()
+                -> new CustomerNotFoundException("Customer could not find by id: " + id));
+    }
+
+}
