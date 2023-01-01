ea39e6a83070abac9594f446e86424cf9c9b358a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-01 21:09:52 +0400

    AccountService

--- src/main/java/com/account/service/AccountService.java
+++ src/main/java/com/account/service/AccountService.java
@@ -1,16 +1,34 @@
 package com.account.service;
 
+import com.account.dto.AccountDto;
+import com.account.dto.CreateAccountRequest;
+import com.account.model.Account;
+import com.account.model.Customer;
 import com.account.repository.AccountRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class AccountService {
 
     private final AccountRepository accountRepository;
+    private final CustomerService customerService;
 
-    public AccountService(AccountRepository accountRepository) {
+    public AccountService(AccountRepository accountRepository, CustomerService customerService) {
         this.accountRepository = accountRepository;
+        this.customerService = customerService;
     }
 
+    public AccountDto createAccount(CreateAccountRequest createAccountRequest) {
+        Customer customer = customerService.findById(createAccountRequest.getCustomerId());
+
+        Account account=new Account(
+                customer,
+                createAccountRequest.getInitialCredit(),
+                LocalDateTime.now()
+        );
+
+    }
 
 }
