2514f4dc816bede57277960e32d19dc214dcd2f7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-01 19:16:31 +0400

    AccountDtoConverter

--- src/main/java/com/account/dto/converter/AccountDtoConverter.java
+++ src/main/java/com/account/dto/converter/AccountDtoConverter.java
@@ -4,8 +4,8 @@ import com.account.dto.AccountDto;
 import com.account.model.Account;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
 import java.util.stream.Collectors;
+
 
 @Component
 public class AccountDtoConverter {
@@ -18,17 +18,24 @@ public class AccountDtoConverter {
         this.transactionDtoConverter = transactionDtoConverter;
     }
 
-    public AccountDto convert(Account account) {
+    public AccountDto accountToAccountDto(Account account) {
+
         return new AccountDto(
                 account.getId(),
                 account.getBalance(),
                 account.getCreationDate(),
-                customerDtoConverter.convertToAccountCustomer(account.getCustomer()),
-                Objects.requireNonNull(account.getTransaction().stream())
-                        .map(transaction -> transactionDtoConverter.convert(transaction))
+                customerDtoConverter.customerToAccountCustomerDto(account.getCustomer()),
+                account.getTransaction()
+                        .stream()
+                        .map(transaction -> transactionDtoConverter.transactionToTransactionDto(transaction))
                         .collect(Collectors.toSet())
-
         );
+
+
     }
+
+
+
+
 
 }
