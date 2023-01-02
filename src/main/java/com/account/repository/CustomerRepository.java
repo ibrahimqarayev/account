bfa1a13f3daeb37914de8719adeeddbcfd5a423d
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-02 12:28:51 +0400

    CustomerRepository

--- src/main/java/com/account/repository/CustomerRepository.java
+++ src/main/java/com/account/repository/CustomerRepository.java
@@ -3,5 +3,6 @@ package com.account.repository;
 import com.account.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CustomerRepository extends JpaRepository<Customer, String> {
 }
