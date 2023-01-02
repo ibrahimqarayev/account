134675e0be3b6fc07e3dc774624e51c342e18f1c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-02 12:27:23 +0400

    AccountApiApplication

--- src/main/java/com/account/AccountApiApplication.java
+++ src/main/java/com/account/AccountApiApplication.java
@@ -1,13 +1,31 @@
 package com.account;
 
+import com.account.model.Customer;
+import com.account.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+
+
 @SpringBootApplication
-public class AccountApiApplication {
+public class AccountApiApplication implements CommandLineRunner {
+
+    private final CustomerRepository customerRepository;
+
+    public AccountApiApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccountApiApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Customer customer = customerRepository.save(new Customer("", "Ibrahim", "Qarayev", new HashSet<>()));
+        System.out.println(customer);
+    }
 }
