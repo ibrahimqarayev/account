7f15bdb8169ba43711e587f6533fdb5e14998d17
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-02 12:27:34 +0400

    Transaction

--- src/main/java/com/account/model/Transaction.java
+++ src/main/java/com/account/model/Transaction.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -22,7 +23,7 @@ public class Transaction {
     private String id;
 
     private TransactionType transactionType;
-//    TransactionType transactionType = TransactionType.INITIAL;
+//    private TransactionType transactionType = TransactionType.INITIAL;
 
     private BigDecimal amount;
 
@@ -32,4 +33,24 @@ public class Transaction {
     @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
+    public Transaction(BigDecimal amount, Account account) {
+        this.id = null;
+        this.transactionType = TransactionType.INITIAL;
+        this.amount = amount;
+        this.transactionDate = LocalDateTime.now();
+        this.account = account;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Objects.equals(id, that.id) && transactionType == that.transactionType && Objects.equals(amount, that.amount) && Objects.equals(transactionDate, that.transactionDate) && Objects.equals(account, that.account);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, transactionType, amount, transactionDate, account);
+    }
 }
