817d42d01af672b0ad6856aecbd10f618f4d2851
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-02 12:27:10 +0400

    AccountService

--- src/main/java/com/account/service/AccountService.java
+++ src/main/java/com/account/service/AccountService.java
@@ -2,11 +2,14 @@ package com.account.service;
 
 import com.account.dto.AccountDto;
 import com.account.dto.CreateAccountRequest;
+import com.account.dto.converter.AccountDtoConverter;
 import com.account.model.Account;
 import com.account.model.Customer;
+import com.account.model.Transaction;
 import com.account.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Service
@@ -14,21 +17,36 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final CustomerService customerService;
+    private final TransactionService transactionService;
+    private final AccountDtoConverter converter;
 
-    public AccountService(AccountRepository accountRepository, CustomerService customerService) {
+
+    public AccountService(AccountRepository accountRepository,
+                          CustomerService customerService,
+                          TransactionService transactionService,
+                          AccountDtoConverter converter) {
         this.accountRepository = accountRepository;
         this.customerService = customerService;
+        this.transactionService = transactionService;
+        this.converter = converter;
     }
 
     public AccountDto createAccount(CreateAccountRequest createAccountRequest) {
         Customer customer = customerService.findById(createAccountRequest.getCustomerId());
 
-        Account account=new Account(
+        Account account = new Account(
                 customer,
                 createAccountRequest.getInitialCredit(),
                 LocalDateTime.now()
         );
 
+        if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
+            Transaction transaction = transactionService.initiateMoney(account, createAccountRequest.getInitialCredit());
+            account.getTransaction().add(transaction);
+        }
+
+        Account savedAccount = accountRepository.save(account);
+        return converter.accountToAccountDto(account);
     }
 
 }
