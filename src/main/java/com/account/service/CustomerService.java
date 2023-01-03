e89627666663998b23c1b93b153ba6e1c1398729
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-03 15:10:42 +0400

    CustomerService

--- src/main/java/com/account/service/CustomerService.java
+++ src/main/java/com/account/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.account.service;
 
+import com.account.dto.CustomerDto;
+import com.account.dto.converter.CustomerDtoConverter;
 import com.account.exception.CustomerNotFoundException;
 import com.account.model.Customer;
 import com.account.repository.CustomerRepository;
@@ -9,14 +11,19 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter customerDtoConverter;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter customerDtoConverter) {
         this.customerRepository = customerRepository;
+        this.customerDtoConverter = customerDtoConverter;
     }
 
-    protected Customer findById(String id) {
+    protected Customer findCustomerById(String id) {
         return customerRepository.findById(id).orElseThrow(()
                 -> new CustomerNotFoundException("Customer could not find by id: " + id));
     }
 
+    public CustomerDto getCustomerById(String customerId) {
+        return customerDtoConverter.customerToCustomerDto(findCustomerById(customerId));
+    }
 }
