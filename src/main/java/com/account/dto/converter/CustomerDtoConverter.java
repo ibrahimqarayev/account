f0d39e90532b2e852e3865b78e264376c9be7e98
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-03 15:10:53 +0400

    CustomerDtoConverter

--- src/main/java/com/account/dto/converter/CustomerDtoConverter.java
+++ src/main/java/com/account/dto/converter/CustomerDtoConverter.java
@@ -1,16 +1,20 @@
 package com.account.dto.converter;
 
 import com.account.dto.AccountCustomerDto;
+import com.account.dto.CustomerDto;
 import com.account.model.Customer;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class CustomerDtoConverter {
 
-    private final TransactionDtoConverter transactionDtoConverter;
 
-    public CustomerDtoConverter(TransactionDtoConverter transactionDtoConverter) {
-        this.transactionDtoConverter = transactionDtoConverter;
+    private final CustomerAccountDtoConverter customerAccountDtoConverter;
+
+    public CustomerDtoConverter(CustomerAccountDtoConverter customerAccountDtoConverter) {
+        this.customerAccountDtoConverter = customerAccountDtoConverter;
     }
 
     public AccountCustomerDto customerToAccountCustomerDto(Customer customer) {
@@ -18,14 +22,24 @@ public class CustomerDtoConverter {
         if (customer == null) {
             return new AccountCustomerDto("", "", "");
         }
-
         return new AccountCustomerDto(
                 customer.getId(),
                 customer.getName(),
                 customer.getSurname()
         );
+    }
 
+    public CustomerDto customerToCustomerDto(Customer customer) {
 
+        return new CustomerDto(
+                customer.getId(),
+                customer.getName(),
+                customer.getSurname(),
+                customer.getAccounts()
+                        .stream()
+                        .map(customerAccountDtoConverter::accountToCustomerAccountDto)
+                        .collect(Collectors.toSet())
+        );
     }
 
 
