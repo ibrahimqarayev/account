b41e2c13355f8ff0f1c24bf77b0df1b6326acb71
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-04 12:21:44 +0400

    moved to company package

--- src/main/java/com/company/account/repository/AccountRepository.java
+++ src/main/java/com/company/account/repository/AccountRepository.java
@@ -1,6 +1,6 @@
-package com.account.repository;
+package com.company.account.repository;
 
-import com.account.model.Account;
+import com.company.account.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
