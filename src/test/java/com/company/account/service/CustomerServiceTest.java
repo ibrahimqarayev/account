d54d0f6bbe10de23df00ac8732c4cb09d93d510f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-06 22:32:57 +0400

    CustomerServiceTest

---
+++ src/test/java/com/company/account/service/CustomerServiceTest.java
@@ -0,0 +1,17 @@
+package com.company.account.service;
+
+import com.company.account.dto.converter.CustomerDtoConverter;
+import com.company.account.repository.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
+public class CustomerServiceTest {
+
+    private CustomerRepository customerRepository;
+    private CustomerDtoConverter customerDtoConverter;
+
+    @BeforeEach
+    void setUp() {
+        customerRepository = Mockito.mock(CustomerRepository.class);
+    }
+}
