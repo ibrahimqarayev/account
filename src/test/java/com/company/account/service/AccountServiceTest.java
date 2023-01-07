1dbbff63314ea3f0789d126ae3e472cad86b4092
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-07 10:40:50 +0400

    AccountServiceTest

--- src/test/java/com/company/account/service/AccountServiceTest.java
+++ src/test/java/com/company/account/service/AccountServiceTest.java
@@ -4,11 +4,10 @@ import com.company.account.dto.converter.AccountDtoConverter;
 import com.company.account.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 public class AccountServiceTest {
 
-
+    private AccountService accountService;
     private AccountRepository accountRepository;
     private CustomerService customerService;
     private AccountDtoConverter accountDtoConverter;
@@ -16,10 +15,10 @@ public class AccountServiceTest {
 
     @BeforeEach
     void setUp() {
-
         accountRepository = Mockito.mock(AccountRepository.class);
         customerService = Mockito.mock(CustomerService.class);
         accountDtoConverter = Mockito.mock(AccountDtoConverter.class);
+        accountService = new AccountService(accountRepository, customerService, accountDtoConverter);
 
     }
 
