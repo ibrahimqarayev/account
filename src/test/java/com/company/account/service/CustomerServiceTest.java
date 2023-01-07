17ac28441281e5a6b23f2c886e634529876f7b5e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-07 10:38:51 +0400

    Test for findCustomerById (Return Customer)

--- src/test/java/com/company/account/service/CustomerServiceTest.java
+++ src/test/java/com/company/account/service/CustomerServiceTest.java
@@ -1,18 +1,37 @@
 package com.company.account.service;
 
+import com.company.account.dto.CustomerDto;
 import com.company.account.dto.converter.CustomerDtoConverter;
+import com.company.account.exception.CustomerNotFoundException;
+import com.company.account.model.Customer;
 import com.company.account.repository.CustomerRepository;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.Optional;
+import java.util.Set;
 
 public class CustomerServiceTest {
 
+    private CustomerService customerService;
     private CustomerRepository customerRepository;
-    private CustomerDtoConverter customerDtoConverter;
+    private CustomerDtoConverter converter;
 
     @BeforeEach
     void setUp() {
         customerRepository = Mockito.mock(CustomerRepository.class);
-        customerDtoConverter = Mockito.mock(CustomerDtoConverter.class);
+        converter = Mockito.mock(CustomerDtoConverter.class);
+        customerService = new CustomerService(customerRepository, converter);
     }
+
+    @Test
+    public void testFindCustomerById_whenCustomerIdExists_shouldReturnCustomer() {
+        Customer customer = new Customer("id", "name", "surname", Set.of());
+        Mockito.when(customerRepository.findById("id")).thenReturn(Optional.of(customer));
+        Customer result = customerService.findCustomerById("id");
+        Assert.assertEquals(result, customer);
+    }
+
 }
