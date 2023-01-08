84440ca571a269edf8e19e88d907ce3c05b7b2d1
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-08 21:23:17 +0400

    AccountControllerTest

--- src/test/java/com/company/account/controller/AccountControllerTest.java
+++ src/test/java/com/company/account/controller/AccountControllerTest.java
@@ -1,4 +1,36 @@
 package com.company.account.controller;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Clock;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+        "server-port=0",
+        "command.line.runner.enabled=false"
+})
+@RunWith(SpringRunner.class)
+@DirtiesContext
 public class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Clock clock;
+
+    @MockBean
+    private Supplier<UUID> uuidSupplier;
+
 }
