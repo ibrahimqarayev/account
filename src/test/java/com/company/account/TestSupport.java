3e2aaa6d5fd828a169e86869a41ff244723c56ea
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-09 23:31:32 +0400

    TestSupport

--- src/test/java/com/company/account/TestSupport.java
+++ src/test/java/com/company/account/TestSupport.java
@@ -1,4 +1,6 @@
 package com.company.account;
 
 public class TestSupport {
+    public static final String CUSTOMER_API_ENDPOINT = "/v1/customer/";
+    public static final String ACCOUNT_API_ENDPOINT = "/v1/account/";
 }
