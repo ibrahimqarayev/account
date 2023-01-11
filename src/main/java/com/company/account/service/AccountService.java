6558c63165489ebdf688c7f60a79ba87e56d796a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-11 20:49:10 +0400

    AccountService

--- src/main/java/com/company/account/service/AccountService.java
+++ src/main/java/com/company/account/service/AccountService.java
@@ -10,7 +10,11 @@ import com.company.account.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.function.Supplier;
 
 @Service
 public class AccountService {
@@ -18,13 +22,17 @@ public class AccountService {
     private final AccountRepository accountRepository;
     private final CustomerService customerService;
     private final AccountDtoConverter converter;
+    private final Clock clock;
+    private final Supplier<UUID> uuidSupplier;
 
     public AccountService(AccountRepository accountRepository,
                           CustomerService customerService,
-                          AccountDtoConverter converter) {
+                          AccountDtoConverter converter, Clock clock, Supplier<UUID> uuidSupplier) {
         this.accountRepository = accountRepository;
         this.customerService = customerService;
         this.converter = converter;
+        this.clock = clock;
+        this.uuidSupplier = uuidSupplier;
     }
 
     public AccountDto createAccount(CreateAccountRequest createAccountRequest) {
@@ -33,8 +41,9 @@ public class AccountService {
         Account account = new Account(
                 customer,
                 createAccountRequest.getInitialCredit(),
-                LocalDateTime.now()
+                getLocalDateTimeNow()
         );
+
 
         if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
             Transaction transaction = new Transaction(
@@ -42,6 +51,19 @@ public class AccountService {
             account.getTransaction().add(transaction);
         }
         return converter.accountToAccountDto(accountRepository.save(account));
+    }
+
+    private LocalDateTime getLocalDateTimeNow() {
+        Instant instant = clock.instant();
+        return LocalDateTime.ofInstant(instant, Clock.systemDefaultZone().getZone());
+    }
+
+    private UUID getUUIDFromString(final String id) {
+        try {
+            return UUID.fromString(id);
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException("Invalid Charging session id: " + id);
+        }
     }
 
 }
