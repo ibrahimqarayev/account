543aee1fa7722775f0d588480728b4c71d8890cf
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-11 22:02:21 +0400

    AccountControllerTest

--- src/test/java/com/company/account/controller/AccountControllerTest.java
+++ src/test/java/com/company/account/controller/AccountControllerTest.java
@@ -1,23 +1,36 @@
 package com.company.account.controller;
 
 import com.company.account.dto.converter.AccountDtoConverter;
+import com.company.account.dto.request.CreateAccountRequest;
+import com.company.account.model.Customer;
 import com.company.account.repository.AccountRepository;
+import com.company.account.repository.CustomerRepository;
 import com.company.account.service.AccountService;
 import com.company.account.service.CustomerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.time.Clock;
 import java.util.UUID;
 import java.util.function.Supplier;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -55,10 +68,38 @@ public class AccountControllerTest {
     @Autowired
     private CustomerService customerService;
     @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
     private AccountDtoConverter converter;
 
     private AccountService accountService = new AccountService(accountRepository, customerService, converter, clock, uuidSupplier);
-    private ObjectMapper mapper;
-    private static  final UUID uuid=UUID.randomUUID();
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setup() {
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
+    private void testCreateAccount_whenCustomerIdExists_shouldCreateAccountAndReturnAccountDto() throws Exception {
+        Customer customer = customerRepository.save(
+                new Customer("Ibrahim", "Qarayev"));
+        CreateAccountRequest request = new CreateAccountRequest(customer.getId(), new BigDecimal(100));
+
+        this.mockMvc.perform(post("/v1/account")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(request)))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.balance", is(100)))
+                .andExpect(jsonPath("$.customer.id", is(customer.getId())))
+                .andExpect(jsonPath("$.customer.name", is("Ibrahim")))
+                .andExpect(jsonPath("$.customer.surname", is("Qarayev")))
+                .andExpect(jsonPath("$.customer.transactions", hasSize(1))
+                );
+
+    }
+
 
 }
