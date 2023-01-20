a939cdc883a283314790118327a8df617f274ab3
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-20 11:28:43 +0400

    unit and integration tests

--- account-api/src/test/java/com/company/account/IntegrationTestSupport.java
+++ account-api/src/test/java/com/company/account/IntegrationTestSupport.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
 public abstract class IntegrationTestSupport extends TestSupport {
+
     @Autowired
     public MockMvc mockMvc;
 
--- account-api/src/test/java/com/company/account/controller/AccountControllerTest.java
+++ account-api/src/test/java/com/company/account/controller/AccountControllerTest.java
@@ -1,72 +1,38 @@
 package com.company.account.controller;
 
-import com.company.account.dto.converter.AccountDtoConverter;
+import com.company.account.IntegrationTestSupport;
 import com.company.account.dto.request.CreateAccountRequest;
 import com.company.account.model.Customer;
-import com.company.account.repository.AccountRepository;
-import com.company.account.repository.CustomerRepository;
-import com.company.account.service.AccountService;
-import com.company.account.service.CustomerService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.math.BigDecimal;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
         "server-port=0",
         "command.line.runner.enabled=false"
 })
+@RunWith(SpringRunner.class)
 @DirtiesContext
-public class AccountControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-
-    @Autowired
-    private AccountRepository accountRepository;
-    @Autowired
-    private CustomerService customerService;
-    @Autowired
-    private CustomerRepository customerRepository;
-    @Autowired
-    private AccountDtoConverter converter;
-
-    private AccountService accountService = new AccountService(accountRepository, customerService, converter);
-    private ObjectMapper mapper = new ObjectMapper();
-
-    @BeforeEach
-    public void setup() {
-        mapper.registerModule(new JavaTimeModule());
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    }
+public class AccountControllerTest extends IntegrationTestSupport {
 
     @Test
-    public void testCreateAccount_whenCustomerIdExists_shouldCreateAccountAndReturnAccountDto() throws Exception {
-        Customer customer = customerRepository.save(
-                new Customer("Ibrahim", "Qarayev"));
-        CreateAccountRequest request = new CreateAccountRequest(customer.getId(), new BigDecimal(100));
+    public void testCreateAccount_whenCustomerIdExits_shouldCreateAccountAndReturnAccountDto() throws Exception {
+        Customer customer = customerRepository.save(generateCustomer());
+        CreateAccountRequest request = generateCreateAccountRequest(customer.getId(), 100);
 
-        this.mockMvc.perform(post("/v1/account")
+        this.mockMvc.perform(post(ACCOUNT_API_ENDPOINT)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(request)))
                 .andExpect(status().is2xxSuccessful())
@@ -74,12 +40,41 @@ public class AccountControllerTest {
                 .andExpect(jsonPath("$.id", notNullValue()))
                 .andExpect(jsonPath("$.balance", is(100)))
                 .andExpect(jsonPath("$.customer.id", is(customer.getId())))
-                .andExpect(jsonPath("$.customer.name", is("Ibrahim")))
-                .andExpect(jsonPath("$.customer.surname", is("Qarayev")))
-                .andExpect(jsonPath("$.transactions", hasSize(1))
-                );
-
+                .andExpect(jsonPath("$.customer.name", is(customer.getName())))
+                .andExpect(jsonPath("$.customer.surname", is(customer.getSurname())))
+                .andExpect(jsonPath("$.transactions", hasSize(1)));
     }
 
+    @Test
+    public void testCreateAccount_whenCustomerIdDoesNotExit_shouldReturn404NotFound() throws Exception {
+        CreateAccountRequest request = generateCreateAccountRequest("id", 100);
+
+        this.mockMvc.perform(post(ACCOUNT_API_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(request)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testCreateAccount_whenRequestHasNoCustomerId_shouldReturn400BadRequest() throws Exception {
+        CreateAccountRequest request = generateCreateAccountRequest("", 100);
+
+        this.mockMvc.perform(post(ACCOUNT_API_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void testCreateAccount_whenRequestHasLessThanZeroInitialCreditValue_shouldReturn400BadRequest() throws Exception {
+        CreateAccountRequest request = generateCreateAccountRequest("id", -100);
+
+        this.mockMvc.perform(post(ACCOUNT_API_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
 
 }
