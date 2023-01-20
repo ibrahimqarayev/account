c5854cf7635284628fae565857f045dbfb770750
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-01-20 11:28:07 +0400

    OpenAPI added

--- account-api/src/main/java/com/company/account/AccountApiApplication.java
+++ account-api/src/main/java/com/company/account/AccountApiApplication.java
@@ -2,10 +2,16 @@ package com.company.account;
 
 import com.company.account.model.Customer;
 import com.company.account.repository.CustomerRepository;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.time.Clock;
 import java.util.HashSet;
 
 
@@ -20,6 +26,23 @@ public class AccountApiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountApiApplication.class, args);
+    }
+
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+                                 @Value("${application-version}") String version) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Account API")
+                        .version(version)
+                        .description(description)
+                        .license(new License().name("Account API Licence")));
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
     }
 
 
